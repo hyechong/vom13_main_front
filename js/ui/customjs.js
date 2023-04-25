5dbec8545fce2341bf386c6c2b4ac23eae1af560
hyechong <ckgpwl2@gmail.com>
2023-04-25 12:46:50 +0900

    best art section 0425

--- js/ui/customjs.js
+++ js/ui/customjs.js
@@ -1,1 +1,11 @@
+/* --------Best Items Slide-------- */
+const swiper = new Swiper('.best-image-wrapper .swiper', {
+  slidesPerView: 4,
+  spaceBetween: 15,
 
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+});

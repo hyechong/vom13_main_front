4fe31aeb7cda03fada0500f52fe0927c7d397d5c
hyechong <ckgpwl2@gmail.com>
2023-05-15 12:47:16 +0900

    로그인, 로그아웃 엔드포인트 추가 0515

--- js/etc/endPoints.js
+++ js/etc/endPoints.js
@@ -1,6 +1,8 @@
 var endPoints = {
   register: {
     signup: 'http://ckgpwl2.dothome.co.kr/baexang_back/register/signup',
+    signin: 'http://ckgpwl2.dothome.co.kr/baexang_back/register/signin',
+    signout: 'http://ckgpwl2.dothome.co.kr/baexang_back/register/signout',
   },
   admin: {},
   productPictures: {},

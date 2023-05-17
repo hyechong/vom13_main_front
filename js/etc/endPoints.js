fa6b065610484693a9bbbd475924e8691912593a
hyechong <ckgpwl2@gmail.com>
2023-05-17 12:46:33 +0900

    로그인 레벨에 따른 헤더 변경, 관리자 페이지 진행중 0517

--- js/etc/endPoints.js
+++ js/etc/endPoints.js
@@ -1,9 +1,11 @@
 var endPoints = {
   register: {
-    signup: 'http://ckgpwl2.dothome.co.kr/baexang_back/register/signup',
-    signin: 'http://ckgpwl2.dothome.co.kr/baexang_back/register/signin',
-    signout: 'http://ckgpwl2.dothome.co.kr/baexang_back/register/signout',
-    isSignin: 'http://ckgpwl2.dothome.co.kr/baexang_back/register/is_signin',
+    signup: 'https://ckgpwl2.cafe24.com/baexang_back/register/signup',
+    signin: 'https://ckgpwl2.cafe24.com/baexang_back/register/signin',
+    signout: 'https://ckgpwl2.cafe24.com/baexang_back/register/signout',
+    isSignin: 'https://ckgpwl2.cafe24.com/baexang_back/register/is_signin',
+    checkAcsCode:
+      'https://ckgpwl2.cafe24.com/baexang_back/register/check_admin_signin',
   },
   admin: {},
   productPictures: {},
